220d26dfa14bba42f22166a5dd926a76aa2ae478
Longo231 <joshlongo91@yahoo.com>
2026-02-20 13:23:00 -0500

    Populates leaderboard based on json formatted file (local for now)

--- LearningLens2025/frontend/lib/Views/leaderboard_view.dart
+++ LearningLens2025/frontend/lib/Views/leaderboard_view.dart
@@ -1,5 +1,8 @@
 
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:learninglens_app/Api/lms/factory/lms_factory.dart';
 import 'package:learninglens_app/Controller/custom_appbar.dart';
 
@@ -8,7 +11,31 @@ class LeaderboardTable extends StatefulWidget {
   _LeaderboardTableState createState() => _LeaderboardTableState();
 }
 
+
 class _LeaderboardTableState extends State<LeaderboardTable> {
+  List<Map<String, dynamic>>? _parsedScores;
+  
+  @override
+  void initState() {
+    super.initState();
+    _loadLeaderboard();
+  }
+
+  Future<void> _loadLeaderboard() async {
+    try {
+      String jsonString = await rootBundle.loadString('assets/GameLeaderboard.txt');
+      List<dynamic> jsonList = jsonDecode(jsonString);
+
+      if (mounted) {
+        setState(() {
+          _parsedScores = jsonList.cast<Map<String, dynamic>>();
+        });
+      }
+    } catch (e) {
+      print("Error while parsing: $e");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,17 +46,45 @@ class _LeaderboardTableState extends State<LeaderboardTable> {
       backgroundColor: Theme.of(context).colorScheme.surface,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
-        children: [Container(
+        children: [
+          Icon(
+            Icons.leaderboard_outlined,
+            size: 120,
+          ),
+          Container(
           margin: const EdgeInsets.all(10),
           child: Table(
             border: TableBorder.all(color: Colors.black, width: 1.0),
             children: [
               // Header Row
               TableRow(children: [
-                TableCell(child: Center(child: Text('Student Name'),)),
-                TableCell(child: Center(child: Text('Game'),)),
-                TableCell(child: Center(child: Text('Score'),)),
-              ])
+                TableCell(child: Center(child: Text('Student Name', style: TextStyle(fontWeight: FontWeight.bold),),)),
+                TableCell(child: Center(child: Text('Game', style: TextStyle(fontWeight: FontWeight.bold),))),
+                TableCell(child: Center(child: Text('Score', style: TextStyle(fontWeight: FontWeight.bold)),)),
+              ]),
+              // Data Rows
+              if (_parsedScores != null && _parsedScores!.isNotEmpty) 
+
+              ..._parsedScores!.asMap().entries.map((entry) {
+                var value = entry.value;
+
+                return TableRow(
+                  children: [
+                    TableCell(child: Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Text(value['student_name'].toString())),
+                    ),
+                    TableCell(child: Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Text(value['game_name'].toString())),
+                    ),
+                    TableCell(child: Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Text(value['score'].toString())),
+                    ),
+                  ]
+                );
+              })
             ],
           )
         )]
